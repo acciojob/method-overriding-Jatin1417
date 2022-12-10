f4dd3f731cdef03cb487b41ae0a3022bc8c560a8
Jatin1417 <chaudhari.jatin.18ee1164@gmail.com>
2022-12-10 23:19:16 +0530

    Second commit

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,6 +3,14 @@ package com.driver.test;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
+import com.driver.Main;
+import com.driver.Main.A;
+import com.driver.Main.B;
+
 public class TestCases {
-    
+    public static void main(String args[]){
+        B bobj = new B();
+        System.out.println(bobj.methA());
+        System.out.println(bobj.meth());
+    }
 }
